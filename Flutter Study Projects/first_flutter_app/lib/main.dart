c7d1e8ff6387ac91344afd502308cd1227ab1fd3
Andy An <nationpower81@gmail.com>
2023-06-12 13:19:22 +0900

    <refactor>(*): Refactor main.dart file
    - Separate  root main code
    - Make a file for draw homepage screen (my_home_page.dart)

--- Flutter Study Projects/first_flutter_app/lib/main.dart
+++ Flutter Study Projects/first_flutter_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart'; //  material 디자인 위젯들이 포함되어 있는 라이브러리
 
+import 'package:first_flutter_app/my_home_page.dart'; // refactoring으로 이동한 화면을 그리는 코드가 위치한 파일 import
+
 // 앱 시작 부분
 void main() {
   runApp(const MyApp()); // runApp() 함수에 MyApp() 인스턴스를 전달한다.
@@ -30,87 +32,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(), // 표시할 화면의 인스턴스
     );
-  }
-}
-
-// 시작 클래스가 실제로 표시하는 클래스 (매인화면)
-/*
-MyHomePage 클래스는 StatefulWidget 클래스를 상속 받은 subclass
-상태가 있는 위젯을 정의하는 StatefulWidget 클래스 사용
- */
-class MyHomePage extends StatefulWidget {
-  // MyHomePage 클래스의 생성자는 key와 title property를 옵션으로 받아서 super키워드를 사용하여 superclass의 생성자에 key를 전달
-  const MyHomePage({super.key});
-
-  @override
-  // MyHomePage 클래스에는 상속 받은 createState() 메서드를 재정의하여 _MyHomePageState() 클래스의 인스턴스를 return
-  // createState() 메서드는 StatefulWidget이 생성될 때 한번만 실행!
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-// MyHomePage 클래스의 상태를 나타내는 State 클래스
-/*
-_MyHomePageState 클래스는 State<MyHomePage> 클래스를 상속 받은 subclass
-State 클래스를 상속 받은 클래스를 상태 클래스라고 한다.
-상태 클래스는 변경 가능한 상태를 property 변수로 표현 하며, 해당 변수의 값을 변경하여 화면을 다시 그릴수 있다.
- */
-class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
-
-  /*
-   화면에 UI를 그리는 메서드 (그려질 위젯을 반환)
-   build() 메서드를 가지고 있으며 이곳에 화면에 그려질 부분을 정의
-   */
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // column 위치 설정 (가운데 정렬)
-          children: [
-            Text(
-              'Number',
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 40,
-              ),
-            ),
-            Text(
-              '$count', // 화면에 표시될 숫자를 count 변수 사용
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 70,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('ElevateButton');
-              },
-              child: Text('ElevateButton'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('TextButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('OutLineButton'),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // 화면 다시 그리기
-          setState(() {
-            count++;
-          });
-        }, // button이 눌렸을때 작동하는 코드를 중괄호 안에 작성
-        child: Icon(Icons.add),
-      ),
-    ); // material 디자인 기본 뼈대 위젯
   }
 }
