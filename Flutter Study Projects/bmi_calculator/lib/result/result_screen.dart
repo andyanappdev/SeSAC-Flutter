44159003d6e859305d17583c2a7a95fb564bcd6e
Andy An <nationpower81@gmail.com>
2023-06-14 01:59:06 +0900

    <feat>(bmi_calculator): Calculate BMI on the result screen to display result and icon

--- Flutter Study Projects/bmi_calculator/lib/result/result_screen.dart
+++ Flutter Study Projects/bmi_calculator/lib/result/result_screen.dart
@@ -10,25 +10,64 @@ class ResultScreen extends StatelessWidget {
     required this.weight,
   });
 
+  // bmi 계산 값을 가지고 결과 문자열을 return하는 함수
+  String _calBmi(double bmi) {
+    String result = '저체중';
+    if (bmi >= 35) {
+      result = '고도 비만';
+    } else if (bmi >= 30) {
+      result = '2단계 비만';
+    } else if (bmi >= 25) {
+      result = '1단계 비만';
+    } else if (bmi >= 23) {
+      result = '과체중';
+    } else if (bmi >= 18.5) {
+      result = '정상';
+    }
+    return result;
+  }
+
+  // bmi 계산 값을 가지고 결과 icon을 return하는 함수
+  Widget _buildIcon(double bmi) {
+    if (bmi >= 23) {
+      return const Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.red,
+        size: 100,
+      );
+    } else if (bmi >= 18.5) {
+      return const Icon(
+        Icons.sentiment_satisfied,
+        color: Colors.green,
+        size: 100,
+      );
+    } else {
+      return const Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.orange,
+        size: 100,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    // BMI 계산 logic
+    final double bmi = weight / ((height / 100.0) * (height / 100.0));
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Result'),
+        title: Text('Result'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '정상',
-              style: TextStyle(fontSize: 36),
+              _calBmi(bmi),
+              style: const TextStyle(fontSize: 36),
             ),
-            Icon(
-              Icons.sentiment_dissatisfied,
-              color: Colors.green,
-              size: 100,
-            ),
+            _buildIcon(bmi),
           ],
         ),
       ),
