47de5243cdcb5fd1a8280baed79678d19e199af7
Andy An <nationpower81@gmail.com>
2023-06-20 00:24:45 +0900

    <feat>(photo_album): Implement ImagePicker for select pictures when entering the app screen
    - override initStat()
    - call Future<void> loadImages()

--- Flutter Study Projects/photo_album/lib/main_screen.dart
+++ Flutter Study Projects/photo_album/lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,6 +9,21 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final ImagePicker _imagePicker = ImagePicker();
+  List<XFile>? images;
+
+  @override
+  void initState() {
+    super.initState();
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    images = await _imagePicker.pickMultiImage();
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
