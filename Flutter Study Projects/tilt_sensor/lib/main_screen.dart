0c5b4a71502fb1293b6735ab59dbfaceb06382f1
Andy An <nationpower81@gmail.com>
2023-06-20 01:56:05 +0900

    <feat>(tilt_sensor): Implement a circular-shape container
    - Use BoxDecoration to make circular-shape container

--- Flutter Study Projects/tilt_sensor/lib/main_screen.dart
+++ Flutter Study Projects/tilt_sensor/lib/main_screen.dart
@@ -15,7 +15,10 @@ class MainScreen extends StatelessWidget {
             left: centerX,
             top: centerY,
             child: Container(
-              color: Colors.red,
+              decoration: const BoxDecoration(
+                color: Colors.green,
+                shape: BoxShape.circle,
+              ),
               width: 100,
               height: 100,
             ),
