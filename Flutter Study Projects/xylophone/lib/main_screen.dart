52c087097497eb9c1babb0e24178a9195e457df8
Andy An <nationpower81@gmail.com>
2023-06-20 04:46:44 +0900

    <feat>(xylophone): Implement a sound to play when a key is tapped

--- Flutter Study Projects/xylophone/lib/main_screen.dart
+++ Flutter Study Projects/xylophone/lib/main_screen.dart
@@ -11,7 +11,8 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   Soundpool pool = Soundpool.fromOptions(options: SoundpoolOptions.kDefault);
-  List<int> _soundIds = []; // sound id들을 담아 두는 list
+  final List<int> _soundIds = []; // sound id들을 담아 두는 list
+  bool _isLoading = true;
 
   @override
   void initState() {
@@ -56,6 +57,10 @@ class _MainScreenState extends State<MainScreen> {
       return pool.load(soundData);
     });
     _soundIds.add(soundId);
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
@@ -68,56 +73,65 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         title: const Text('Play Xylophone'),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: gunban('도', Colors.red),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24.0),
-            child: gunban('레', Colors.orange),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32.0),
-            child: gunban('미', Colors.indigo),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40.0),
-            child: gunban('파', Colors.yellow),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 48.0),
-            child: gunban('솔', Colors.cyan),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 56.0),
-            child: gunban('라', Colors.blue),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 64.0),
-            child: gunban('시', Colors.green),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 64.0),
-            child: gunban('도', Colors.purple),
-          ),
-        ],
-      ),
+      body: _isLoading // 만약 로딩중이라면 indicator 표시하고 아니면 건반 표시시
+          ? const Center(
+              child: CircularProgressIndicator(), // loading indicator 생성
+            )
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: gunban('도', Colors.red, _soundIds[0]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: gunban('레', Colors.orange, _soundIds[1]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 32.0),
+                  child: gunban('미', Colors.indigo, _soundIds[2]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40.0),
+                  child: gunban('파', Colors.yellow, _soundIds[3]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 48.0),
+                  child: gunban('솔', Colors.cyan, _soundIds[4]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 56.0),
+                  child: gunban('라', Colors.blue, _soundIds[5]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 64.0),
+                  child: gunban('시', Colors.green, _soundIds[6]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 72.0),
+                  child: gunban('도', Colors.purple, _soundIds[7]),
+                ),
+              ],
+            ),
     );
   }
 
-  Widget gunban(String text, Color color) {
-    return Container(
-        width: 50,
-        height: double.infinity,
-        color: color,
-        child: Center(
-          child: Text(
-            '$text',
-            style: const TextStyle(color: Colors.white),
-          ),
-        ));
+  Widget gunban(String text, Color color, int soundId) {
+    return GestureDetector(
+      onTap: () {
+        pool.play(soundId);
+      },
+      child: Container(
+          width: 50,
+          height: double.infinity,
+          color: color,
+          child: Center(
+            child: Text(
+              text,
+              style: const TextStyle(color: Colors.white),
+            ),
+          )),
+    );
   }
 }
