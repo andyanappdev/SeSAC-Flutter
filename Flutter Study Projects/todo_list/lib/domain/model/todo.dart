f9ac12673b7e48f2a80c0a1502fb806864d1c1b2
Andy An <nationpower81@gmail.com>
2023-06-20 20:18:04 +0900

    <feat>(todo_list): Add hive_flutter package and to setup to use hive box

--- Flutter Study Projects/todo_list/lib/domain/model/todo.dart
+++ Flutter Study Projects/todo_list/lib/domain/model/todo.dart
@@ -1,5 +1,16 @@
-class Todo {
+import 'package:hive/hive.dart';
+part 'todo.g.dart';
+
+@HiveType(typeId: 0) // hive를 이용하여 저장할 객체에 annotation 달기
+class Todo extends HiveObject {
+  // HiveObject 상속
+  @HiveField(0) // field에도 annotation 달기
+  int? id;
+
+  @HiveField(1)
   String title;
+
+  @HiveField(2)
   int dateTime;
 
   Todo({required this.title, required this.dateTime});
--- Flutter Study Projects/todo_list/lib/main.dart
+++ Flutter Study Projects/todo_list/lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:todo_list/domain/model/todo.dart';
 import 'package:todo_list/presentation/todo_list_screen.dart';
 
-void main() {
+// top level 변수
+late final Box<Todo> todos;
+
+void main() async {
+  await Hive.initFlutter(); // hive 초기화
+  Hive.registerAdapter(TodoAdapter());
+  Box<Todo> todos = await Hive.openBox<Todo>('todolist.db');
   runApp(const MyApp());
 }
 
