1bc973a641b9dd6043510a14725882c544184054
Andy An <nationpower81@gmail.com>
2023-06-20 13:33:07 +0900

    <feat>(gps_map): Add geolocator package

--- Flutter Study Projects/gps_map/lib/main_screen.dart
+++ Flutter Study Projects/gps_map/lib/main_screen.dart
@@ -14,6 +14,7 @@ class _MainScreenState extends State<MainScreen> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
+  // The position of the map "camera", the view point from which the world is shown in the map view.
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -29,7 +30,7 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
