0f0d37775dce5e0d16c5c0c075083667df54a67e
Andy An <nationpower81@gmail.com>
2023-06-26 18:36:38 +0900

    <feat>(statefullwidget_setstate): Implement Future with setState

--- Flutter Projects/Flutter Class Projects/statefullwidget_setstate/lib/presentation/future_screen.dart
+++ Flutter Projects/Flutter Class Projects/statefullwidget_setstate/lib/presentation/future_screen.dart
@@ -8,8 +8,53 @@ class FutureScreen extends StatefulWidget {
 }
 
 class _FutureScreenState extends State<FutureScreen> {
+  String text = '이곳에 표시!!!';
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Future with setState'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (isLoading == true)
+              Text(
+                'Loading',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+            if (isLoading == false)
+              Text(
+                text,
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: ElevatedButton(
+                onPressed: () async {
+                  isLoading = true;
+                  // 화면 갱신
+                  setState(() {});
+
+                  text = await getString();
+                  isLoading = false;
+                  // 화면 갱신
+                  setState(() {});
+                },
+                child: const Text('Future 실행'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
+}
+
+Future<String> getString() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 'Hello Future';
 }
