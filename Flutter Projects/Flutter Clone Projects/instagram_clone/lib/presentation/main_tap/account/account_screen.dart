9d6ea7d1a6d99ff7dabf261762e0d5886c81fa15
Andy An <nationpower81@gmail.com>
2023-06-26 23:05:55 +0900

    <feat>(instagram_clone): Implement AccountScreen UI

--- Flutter Projects/Flutter Clone Projects/instagram_clone/lib/presentation/main_tap/account/account_screen.dart
+++ Flutter Projects/Flutter Clone Projects/instagram_clone/lib/presentation/main_tap/account/account_screen.dart
@@ -7,7 +7,76 @@ class AccountScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Account'),
+        title: const Text('Instagram Clone'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    const SizedBox(
+                      height: 80,
+                      width: 80,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://newsimg.sedaily.com/2023/05/10/29PIXQDV1Q_1.jpg'),
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      alignment: Alignment.bottomRight,
+                      child: const SizedBox(
+                        width: 25,
+                        height: 25,
+                        child: FloatingActionButton(
+                          onPressed: null,
+                          child: Icon(Icons.add_circle),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  'IU',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text('3', style: TextStyle(fontSize: 18)),
+                Text('게시물', style: TextStyle(fontSize: 18)),
+              ],
+            ),
+            const Column(
+              children: [
+                Text('0', style: TextStyle(fontSize: 18)),
+                Text('팔로우', style: TextStyle(fontSize: 18)),
+              ],
+            ),
+            const Column(
+              children: [
+                Text('0', style: TextStyle(fontSize: 18)),
+                Text('팔로잉', style: TextStyle(fontSize: 18)),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
