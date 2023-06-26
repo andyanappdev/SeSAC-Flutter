0223d7b73df74282abf4e170ec3bdf2548fa4c99
Andy An <nationpower81@gmail.com>
2023-06-26 17:02:23 +0900

    <feat>(instagram_clone): Instagram Clone project first commit

---
+++ Flutter Projects/Flutter Clone Projects/instagram_clone/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/presentation/main_tap/main_tap_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram Clone',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainTapScreen(),
+    );
+  }
+}
---
+++ Flutter Projects/Flutter Clone Projects/instagram_clone/lib/presentation/main_tap/main_tap_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainTapScreen extends StatefulWidget {
+  const MainTapScreen({super.key});
+
+  @override
+  State<MainTapScreen> createState() => _MainTapScreenState();
+}
+
+class _MainTapScreenState extends State<MainTapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
