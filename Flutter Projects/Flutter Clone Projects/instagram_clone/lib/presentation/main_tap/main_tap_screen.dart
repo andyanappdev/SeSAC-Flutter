b4924ccc1d5f418cfa68e79053dc9618f22472fc
Andy An <nationpower81@gmail.com>
2023-06-26 18:54:01 +0900

    <feat>(instagram_clone): Implement BottomNavigationBar

--- Flutter Projects/Flutter Clone Projects/instagram_clone/lib/presentation/main_tap/main_tap_screen.dart
+++ Flutter Projects/Flutter Clone Projects/instagram_clone/lib/presentation/main_tap/main_tap_screen.dart
@@ -8,8 +8,35 @@ class MainTapScreen extends StatefulWidget {
 }
 
 class _MainTapScreenState extends State<MainTapScreen> {
+  int _currentIndex = 0; // BottomNavigationBar의 currentIndex 값을 담아둘 변수 생성
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        // bottomNavigationBar의 아이템이 눌렷을때
+        onTap: (index) {
+          setState(() {
+            _currentIndex =
+                index; // BottomNavigationBar 눌린 아이템의 index값을 _currentIndex에 할당하고 화면갱신 하여 반영
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Login',
+          ),
+        ],
+      ),
+    );
   }
 }
