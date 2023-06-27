eb1dcd83ea94453366e28cf186e812b9d46eec79
Andy An <nationpower81@gmail.com>
2023-06-27 13:00:10 +0900

    <fix>(instagram_clone): Fix Android Gradle plugin supports only Kotlin Gradle plugin version 1.5.20 and higher Error
    - External Libraries → Flutter Plugins → sign_in_apple-3.3.0 → android → build.gradle file : ext.kotlin_version = '1.3.50' set to ‘1.7.10’

--- Flutter Projects/Flutter Clone Projects/instagram_clone/lib/main.dart
+++ Flutter Projects/Flutter Clone Projects/instagram_clone/lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:instagram_clone/presentation/main_tap/main_tap_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/presentation/auth/auth_gate.dart';
+
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainTapScreen(),
+      home: const AuthGagte(),
     );
   }
 }
