940ed4f57e9206f679d66f0995c023f539bd654d
Andy An <nationpower81@gmail.com>
2023-06-28 02:25:34 +0900

    <feat>(instagram_clone): Implement on AccountScreen to show posts created by user

--- Flutter Projects/Flutter Clone Projects/instagram_clone/lib/presentation/main_tap/account/account_model.dart
+++ Flutter Projects/Flutter Clone Projects/instagram_clone/lib/presentation/main_tap/account/account_model.dart
@@ -1,6 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_clone/domain/model/post.dart';
 
 class AccountModel {
+  final Stream<QuerySnapshot<Post>> postsStream = FirebaseFirestore.instance
+      .collection('posts')
+      .where('userId', isEqualTo: FirebaseAuth.instance.currentUser?.uid)
+      .withConverter<Post>(
+        fromFirestore: (snapshot, _) => Post.fromJson(snapshot.data()!),
+        toFirestore: (post, _) => post.toJson(),
+      )
+      .snapshots();
+
   Future<void> logout() async {
     await FirebaseAuth.instance.signOut();
   }
--- Flutter Projects/Flutter Clone Projects/instagram_clone/lib/presentation/main_tap/account/account_screen.dart
+++ Flutter Projects/Flutter Clone Projects/instagram_clone/lib/presentation/main_tap/account/account_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/domain/model/post.dart';
+import 'package:instagram_clone/presentation/detail_post/detail_post_screen.dart';
 import 'package:instagram_clone/presentation/main_tap/account/account_model.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -21,72 +24,128 @@ class AccountScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Column(
           children: [
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Stack(
+                Column(
                   children: [
-                    SizedBox(
-                      height: 80,
-                      width: 80,
-                      child: CircleAvatar(
-                        backgroundImage:
-                            NetworkImage(model.getProfileImageUrl()),
+                    Stack(
+                      children: [
+                        SizedBox(
+                          height: 80,
+                          width: 80,
+                          child: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(model.getProfileImageUrl()),
+                          ),
+                        ),
+                        Container(
+                          width: 80,
+                          height: 80,
+                          alignment: Alignment.bottomRight,
+                          child: const SizedBox(
+                            width: 25,
+                            height: 25,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.add_circle),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      model.getNickName(),
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Container(
-                      width: 80,
-                      height: 80,
-                      alignment: Alignment.bottomRight,
-                      child: const SizedBox(
-                        width: 25,
-                        height: 25,
-                        child: FloatingActionButton(
-                          onPressed: null,
-                          child: Icon(Icons.add_circle),
-                        ),
-                      ),
-                    )
                   ],
                 ),
-                const SizedBox(height: 8),
-                Text(
-                  model.getNickName(),
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
+                const Column(
+                  children: [
+                    Text('3', style: TextStyle(fontSize: 18)),
+                    Text(
+                      '게시물',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const Column(
+                  children: [
+                    Text('0', style: TextStyle(fontSize: 18)),
+                    Text(
+                      '팔로우',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const Column(
+                  children: [
+                    Text('0', style: TextStyle(fontSize: 18)),
+                    Text(
+                      '팔로잉',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
               ],
             ),
-            const Column(
-              children: [
-                Text('3', style: TextStyle(fontSize: 18)),
-                Text(
-                  '게시물',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            const Column(
-              children: [
-                Text('0', style: TextStyle(fontSize: 18)),
-                Text(
-                  '팔로우',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            const Column(
-              children: [
-                Text('0', style: TextStyle(fontSize: 18)),
-                Text(
-                  '팔로잉',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ],
+            const SizedBox(height: 15),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: StreamBuilder<QuerySnapshot<Post>>(
+                    stream: model.postsStream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('Error: unknown error');
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      List<Post> posts =
+                          snapshot.data!.docs.map((e) => e.data()).toList();
+
+                      return GridView.builder(
+                          itemCount: posts.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            mainAxisSpacing: 2.0,
+                            crossAxisSpacing: 2.0,
+                          ), // GridView의 열 갯수 설정
+                          itemBuilder: (BuildContext context, int index) {
+                            final post = posts[index];
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          DetailPostScreen(post: post)),
+                                );
+                              },
+                              child: Hero(
+                                tag: post.id,
+                                child: Image.network(
+                                  post.imageUrl,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          });
+                    }),
+              ),
             ),
           ],
         ),
