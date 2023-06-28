5759dbbc2f93ad02755e7cb2c6d4da04fb7094bb
Andy An <nationpower81@gmail.com>
2023-06-29 03:43:10 +0900

    <refactor>(xylophone): Set up to apply MVVM

--- Flutter Projects/Flutter Study Projects/xylophone/lib/main.dart
+++ Flutter Projects/Flutter Study Projects/xylophone/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:xylophone/main_screen.dart';
+import 'package:xylophone/presentation/main/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
