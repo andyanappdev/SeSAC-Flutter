f1cb7a9d7e93254eb7f98946529c383f513f4dae
Andy An <nationpower81@gmail.com>
2023-07-06 00:20:01 +0900

    <feat>(provider_practice): Implement ChangeNotifierProvider to display count number on MainScreen
    - To display count number on MainScreen when notified of a change in the count state of ViewModel

--- Flutter Projects/Flutter Class Projects/provider_practice/lib/presentation/main/main_screen.dart
+++ Flutter Projects/Flutter Class Projects/provider_practice/lib/presentation/main/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_practice/presentation/main/main_view_model.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,6 +12,28 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final viewModel = context.watch<MainViewModel>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: Text(
+          '${viewModel.count}',
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              context.read<MainViewModel>().increment();
+            },
+            child: const Text('+'),
+          ),
+        ],
+      ),
+    );
   }
 }
--- Flutter Projects/Flutter Class Projects/provider_practice/lib/presentation/main/main_view_model.dart
+++ Flutter Projects/Flutter Class Projects/provider_practice/lib/presentation/main/main_view_model.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
+//  ChangeNotifier(mixin type : with 사용)를 상속 받아서 구현하여 변경되는 상태를 ChangeNotifierProvider에게 통지해 줄수 있다.
 class MainViewModel with ChangeNotifier {
   int count = 0;
 
   void increment() {
     count++;
-    notifyListeners(); // listener 설정
+    notifyListeners(); // listener() 메서드를 호출하여 상태 변경 사항을 provider에게 통지
   }
 }
