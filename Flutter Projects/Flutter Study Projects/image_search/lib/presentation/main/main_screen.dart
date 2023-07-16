99c26e6b654601b914460b90a1323bf1dc673bf2
Andy An <nationpower81@gmail.com>
2023-07-16 20:28:22 +0900

    <feat>(image_search): Make layout of MainScreen UI

--- Flutter Projects/Flutter Study Projects/image_search/lib/presentation/main/main_screen.dart
+++ Flutter Projects/Flutter Study Projects/image_search/lib/presentation/main/main_screen.dart
@@ -7,10 +7,57 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Image Search App'),
+        centerTitle: true,
+        title: const Text(
+          'Image Search App',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.grey,
+        elevation: 0.0, // appbar 그림자 제거
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    // 클릭시 작동 코드 작성
+                  },
+                  icon: const Icon(Icons.search),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemCount: 10,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisExtent: 16,
+              ),
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16.0),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          'https://i.namu.wiki/i/R0AhIJhNi8fkU2Al72pglkrT8QenAaCJd1as-d_iY6MC8nub1iI5VzIqzJlLa-1uzZm--TkB-KHFiT-P-t7bEg.webp'),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
-    ;
   }
 }
