43c939d72b8a842db5769413e77c96ce5da75422
Andy An <nationpower81@gmail.com>
2023-07-18 22:25:35 +0900

    <refactor>(image_search): Make State class an immutabel object by Freezed package

--- Flutter Projects/Flutter Study Projects/Inflearn Lectures/05. Clean Architecture/image_search/lib/presentation/main/main_state.dart
+++ Flutter Projects/Flutter Study Projects/Inflearn Lectures/05. Clean Architecture/image_search/lib/presentation/main/main_state.dart
@@ -1,19 +1,37 @@
 import 'package:image_search/domain/model/photo.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
 
-class MainState {
-  final List<Photo> photos;
-  final bool isLoading;
+part 'main_state.freezed.dart';
 
-  MainState({
-    required this.photos,
-    required this.isLoading,
-  });
+part 'main_state.g.dart';
 
-  // deepCopy
-  MainState copy({List<Photo>? photos, bool? isLoading}) {
-    return MainState(
-      photos: photos ?? this.photos,
-      isLoading: isLoading ?? this.isLoading,
-    );
-  }
+@freezed
+class MainState with _$MainState {
+  const factory MainState(
+    List<Photo> photos,
+    bool isLoading,
+  ) = _MainState;
+
+  factory MainState.fromJson(Map<String, Object?> json) =>
+      _$MainStateFromJson(json);
 }
+
+/// 수동으로 State class 생성
+// class MainState {
+//   final List<Photo> photos;
+//   final bool isLoading;
+//
+//   MainState({
+//     required this.photos,
+//     required this.isLoading,
+//   });
+//
+//   // deepCopy
+//   MainState copy({List<Photo>? photos, bool? isLoading}) {
+//     return MainState(
+//       photos: photos ?? this.photos,
+//       isLoading: isLoading ?? this.isLoading,
+//     );
+//   }
+// }
--- Flutter Projects/Flutter Study Projects/Inflearn Lectures/05. Clean Architecture/image_search/lib/presentation/main/main_view_model.dart
+++ Flutter Projects/Flutter Study Projects/Inflearn Lectures/05. Clean Architecture/image_search/lib/presentation/main/main_view_model.dart
@@ -1,42 +1,42 @@
 import 'dart:async';
-import 'dart:collection';
 
 import 'package:flutter/cupertino.dart';
 import 'package:image_search/data/data_source/result.dart';
-import 'package:image_search/domain/repository/photo_api_repository.dart';
 import 'package:image_search/domain/model/photo.dart';
+import 'package:image_search/domain/repository/photo_api_repository.dart';
 import 'package:image_search/presentation/main/main_state.dart';
 import 'package:image_search/presentation/main/main_ui_event.dart';
 
 class MainViewModel with ChangeNotifier {
   final PhotoApiRepository repository;
 
-  MainState _state = MainState(photos: [], isLoading: false);
+  MainState _state = const MainState([], false);
   // getter
   MainState get state => _state;
 
   final StreamController<MainUiEvent> _eventController =
       StreamController<MainUiEvent>();
+  // getter
   Stream<MainUiEvent> get eventStream => _eventController.stream;
 
   MainViewModel(this.repository);
 
   Future<void> fetch(String query) async {
-    _state = _state.copy(isLoading: true);
+    _state = _state.copyWith(isLoading: true);
     notifyListeners();
 
     final Result<List<Photo>> result = await repository.fetch(query);
 
     switch (result) {
       case Success(:final data):
-        _state = _state.copy(photos: data);
+        _state = _state.copyWith(photos: data);
         notifyListeners();
       case Error(:final e):
         // error 처리 코드
         _eventController.add(MainUiEvent.showSnackBar(e));
     }
 
-    _state = _state.copy(isLoading: false);
+    _state = _state.copyWith(isLoading: false);
     notifyListeners();
   }
 }
