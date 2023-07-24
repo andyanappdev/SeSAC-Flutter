1a214b17bc9e4b55755689a7789dff33ae3a1c03
Andy An <nationpower81@gmail.com>
2023-07-24 13:23:24 +0900

    <feat>(injectable_practice): Complete MainScreen

--- Flutter Projects/Flutter Class Projects/injectable_practice/lib/ui/main/main_screen.dart
+++ Flutter Projects/Flutter Class Projects/injectable_practice/lib/ui/main/main_screen.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:injectable_practice/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('MainScreen'),
       ),
-      body: Container(),
+      body: Center(
+        child: Text(
+          '${viewModel.count}',
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              context.read<MainViewModel>().increment();
+            },
+            child: const Text('+'),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              context.push('/second');
+            },
+            child: const Text(
+              'Next Screen',
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
