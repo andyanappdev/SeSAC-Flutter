4dc840cf25e30dd1e0f1fa319dbdc22de7c6169c
Andy An <nationpower81@gmail.com>
2023-07-25 23:31:03 +0900

    <feat>(get_mask_clean_arch): Implement GetNearByStoresUseCase class

--- Flutter Projects/Flutter Study Projects/Inflearn Lectures/06. Dependency Injection/get_mask_clean_arch/lib/domain/model/location.dart
+++ Flutter Projects/Flutter Study Projects/Inflearn Lectures/06. Dependency Injection/get_mask_clean_arch/lib/domain/model/location.dart
@@ -1,6 +1,13 @@
 class Location {
-  final double latitude;
-  final double longitude;
+  final num latitude;
+  final num longitude;
 
   Location(this.latitude, this.longitude);
+}
+
+// 거리를 계산하는 logic method (Extension methods)
+extension DistanceBetween on Location {
+  num distanceBetween(Location other) {
+    return 0.0;
+  }
 }
