98c08a8326401466d6b728661f068c754be13b7a
Andy An <nationpower81@gmail.com>
2023-07-26 00:41:54 +0900

    <feat>(get_mask_clean_arch): Create MainViewModel class

---
+++ Flutter Projects/Flutter Study Projects/Inflearn Lectures/06. Dependency Injection/get_mask_clean_arch/lib/presentation/main/main_view_model.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get_mask_clean_arch/domain/use_case/get_near_by_stores_use_case.dart';
+import 'package:get_mask_clean_arch/presentation/main/main_state.dart';
+
+class MainViewModel with ChangeNotifier {
+  final GetNearByStoresUseCase _getNearByStoresUseCase;
+
+  MainState _state = const MainState();
+
+  // getter
+  MainState get state => _state;
+
+  MainViewModel(this._getNearByStoresUseCase);
+
+  Future<void> getStores() async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+
+    _state = state.copyWith(
+      stores: await _getNearByStoresUseCase.execute(),
+      isLoading: false,
+    );
+    notifyListeners();
+  }
+}
