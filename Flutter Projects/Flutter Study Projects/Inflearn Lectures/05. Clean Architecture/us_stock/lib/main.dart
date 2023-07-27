3f1bda96b1554544bd8a13f9c2be5b205533b665
Andy An <nationpower81@gmail.com>
2023-07-27 22:06:04 +0900

    <feat>(us_stock): US Stock project first commit
    - Setup Packages
    - dependencies (http, go_router, provider, freezed_annotation, json_annotation, get_it, injectable, hive, hive_flutter)
    - dev_dependencies (freezed, json_serializable, build_runner, test, injectable_generator)
    - Theme Setup

---
+++ Flutter Projects/Flutter Study Projects/Inflearn Lectures/05. Clean Architecture/us_stock/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:us_stock/color_schemes.g.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: lightColorScheme,
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: darkColorScheme,
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
+      home: const MainScreen(),
+    );
+  }
+}
